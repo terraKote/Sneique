5e2db49805e4a34c364d194dbf603ecc1628644e
Denis Kushnirenko <den4ik.kushnirenko@gmail.com>
2024-11-10 02:25:49 +0200

    Restored snake movement

--- Game/SnakeObject.cpp
+++ Game/SnakeObject.cpp
@@ -3,76 +3,35 @@
 
 void SnakeObject::Update(double deltaTime) {
 	if (_inputManager->IsButtonPressed(UP)) {
+		_velocity.setX(0);
+		_velocity.setY(-1);
 		_currentDirection = Direction::Up;
 	}
-
-	if (_inputManager->IsButtonPressed(DOWN)) {
+	else if (_inputManager->IsButtonPressed(DOWN)) {
+		_velocity.setX(0);
+		_velocity.setY(1);
 		_currentDirection = Direction::Down;
 	}
-
-	if (_inputManager->IsButtonPressed(LEFT)) {
+	else if (_inputManager->IsButtonPressed(LEFT)) {
+		_velocity.setX(-1);
+		_velocity.setY(0);
 		_currentDirection = Direction::Left;
 	}
-
-	if (_inputManager->IsButtonPressed(RIGHT)) {
+	else if (_inputManager->IsButtonPressed(RIGHT)) {
+		_velocity.setX(1);
+		_velocity.setY(0);
 		_currentDirection = Direction::Right;
 	}
 
-	//case SDL_KEYDOWN:
-	//	SDL_KeyboardEvent* keybevt = (SDL_KeyboardEvent*)&evt;
-	//	switch (keybevt->keysym.sym)
-	//	{
-	//	case SDLK_UP:
-	//		velocity.setX(0);
-	//		velocity.setY(-1);
-	//		currentDirection = Direction::Up;
-	//		break;
+	_currentMoveTime += deltaTime;
 
-	//	case SDLK_LEFT:
-	//		velocity.setX(-1);
-	//		velocity.setY(0);
-	//		currentDirection = Direction::Left;
-	//		break;
+	if (_currentMoveTime < _moveTime)
+		return;
 
-	//	case SDLK_DOWN:
-	//		velocity.setX(0);
-	//		velocity.setY(1);
-	//		currentDirection = Direction::Down;
-	//		break;
-
-	//	case SDLK_RIGHT:
-	//		velocity.setX(1);
-	//		velocity.setY(0);
-	//		currentDirection = Direction::Right;
-	//		break;
-	//	}
-	//	break;
-
-	//	//-----------
-
-	//	currentMoveTime += deltaTime;
-
-	//	if (currentMoveTime >= moveTime) {
-	//		snakeBody.pop_back();
-	//		Vectormath::Vector2 headPosition = { snakeBody[0].getX() + velocity.getX() * UNIT_SIZE, snakeBody[0].getY() + velocity.getY() * UNIT_SIZE };
-	//		snakeBody.push_front(headPosition);
-
-	//		for (unsigned int i = 0; i < snakeBody.size(); i++) {
-	//			Vectormath::Vector2 point = snakeBody[i];
-
-	//			int spriteIndex = currentDirection;
-
-	//			if (i != 0)
-	//			{
-	//				spriteIndex = 4;
-	//			}
-
-	//			TLN_SetSpritePicture(i, spriteIndex);
-	//			TLN_SetSpritePosition(i, point.getX(), point.getY());
-	//		}
-
-	//		currentMoveTime = 0;
-	//	}
+	_snakeBody.pop_back();
+	Vectormath::Vector2 headPosition = { _snakeBody[0].getX() + _velocity.getX() * UNIT_SIZE, _snakeBody[0].getY() + _velocity.getY() * UNIT_SIZE };
+	_snakeBody.push_front(headPosition);
+	_currentMoveTime = 0;
 
 	//	if (snakeBody[0].getX() == fruitPosition.getX() && snakeBody[0].getY() == fruitPosition.getY())
 	//	{
